2b37192f11542b9b7c4a752bfb0431e65b2aa609
Leon Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2020-03-16 15:27:46 +0000

    Cleanup / minor refactor

--- src/draw.c
+++ src/draw.c
@@ -26,27 +26,31 @@
 #include"lavalauncher.h"
 #include"draw.h"
 
-/* Draw the icons for all defined buttons. */
-// TODO remove conditional in loop
-// TODO Draw once on startup to a cairo_surface (and at scale updates)
+// TODO Draw icons once on startup to a cairo_surface (and at scale updates)
 // TODO Possibly draw icons to a subsurface so only the bar needs redrawing for animations
 // TODO Use something faster than cairo for drawing the icons
+
+static void draw_single_icon (cairo_t *cairo, int32_t x, int32_t y,
+		int32_t icon_size, cairo_surface_t *img)
+{
+	float w = cairo_image_surface_get_width(img);
+	float h = cairo_image_surface_get_height(img);
+
+	cairo_save(cairo);
+	cairo_translate(cairo, x, y);
+	cairo_scale(cairo, icon_size / w, icon_size / h);
+	cairo_set_source_surface(cairo, img, 0, 0);
+	cairo_paint(cairo);
+	cairo_restore(cairo);
+}
+
 static void draw_icons (cairo_t *cairo, int32_t x, int32_t y, int32_t icon_size,
 		enum Bar_orientation orientation, struct wl_list *button_list)
 {
-	float w, h;
 	struct Lava_button *bt_1, *bt_2;
 	wl_list_for_each_reverse_safe(bt_1, bt_2, button_list, link)
 	{
-		w = cairo_image_surface_get_width(bt_1->img);
-		h = cairo_image_surface_get_height(bt_1->img);
-
-		cairo_save(cairo);
-		cairo_translate(cairo, x, y);
-		cairo_scale(cairo, icon_size / w, icon_size / h);
-		cairo_set_source_surface(cairo, bt_1->img, 0, 0);
-		cairo_paint(cairo);
-		cairo_restore(cairo);
+		draw_single_icon(cairo, x, y, icon_size, bt_1->img);
 
 		if ( orientation == ORIENTATION_HORIZONTAL )
 			x += icon_size;
@@ -55,7 +59,6 @@ static void draw_icons (cairo_t *cairo, int32_t x, int32_t y, int32_t icon_size,
 	}
 }
 
-/* Draw a coloured rectangle. */
 static void draw_coloured_rectangle (cairo_t *cairo, float colour[4],
 		int x, int y, int w, int h, float scale)
 {
@@ -91,35 +94,43 @@ static void draw_bar (cairo_t *cairo, int32_t x, int32_t y, int32_t w, int32_t h
 			border_left, h - border_top - border_bottom, scale);
 }
 
+static void clear_cairo_buffer (cairo_t *cairo)
+{
+	cairo_save(cairo);
+	cairo_set_operator(cairo, CAIRO_OPERATOR_CLEAR);
+	cairo_paint(cairo);
+	cairo_restore(cairo);
+}
+
+static void calculate_buffer_size (struct Lava_data *data, struct Lava_output *output,
+		int *w, int *h)
+{
+	if ( data->orientation == ORIENTATION_HORIZONTAL )
+	{
+		*w = output->w;
+		*h = data->h * output->scale;
+	}
+	else
+	{
+		*w = data->w * output->scale;
+		*h = output->h;
+	}
+}
+
 void render_bar_frame (struct Lava_data *data, struct Lava_output *output)
 {
 	if (! output->configured)
 		return;
 
-	/* Calculate buffer size. */
-	int buffer_w, buffer_h;
-	if ( data->orientation == ORIENTATION_HORIZONTAL )
-	{
-		buffer_w = output->w;
-		buffer_h = data->h * output->scale;
-	}
-	else
-	{
-		buffer_w = data->w * output->scale;
-		buffer_h = output->h;
-	}
-
 	/* Get new/next buffer. */
+	int buffer_w, buffer_h;
+	calculate_buffer_size(data, output, &buffer_w, &buffer_h);
 	if (! next_buffer(&output->current_buffer, data->shm, output->buffers,
 				buffer_w, buffer_h))
 		return;
 
-	/* Clear buffer. */
 	cairo_t *cairo = output->current_buffer->cairo;
-	cairo_save(cairo);
-	cairo_set_operator(cairo, CAIRO_OPERATOR_CLEAR);
-	cairo_paint(cairo);
-	cairo_restore(cairo);
+	clear_cairo_buffer(cairo);
 
 	/* Draw bar. */
 	if (data->verbose)

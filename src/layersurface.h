8f9cea1ce669d15eee4fde371ded77b4e5dd9caf
Leon Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2020-04-01 18:42:02 +0200

    Move layershell operation into separate file

---
+++ src/layersurface.h
@@ -0,0 +1,9 @@
+#ifndef LAVALAUNCHER_LAYERSURFACE_H
+#define LAVALAUNCHER_LAYERSURFACE_H
+
+#include"lavalauncher.h"
+
+bool create_bar (struct Lava_data *data, struct Lava_output *output);
+void configure_surface (struct Lava_data *data, struct Lava_output *output);
+
+#endif

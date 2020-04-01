2c1d5f69575a4d4ba468aaae0d9f8d172e1c94a1
Leon Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2020-04-01 19:02:16 +0200

    Move output operations to separate file

--- src/lavalauncher.c
+++ src/lavalauncher.c
@@ -43,6 +43,7 @@
 #include"layersurface.h"
 #include"draw.h"
 #include"seat.h"
+#include"output.h"
 
 static const char usage[] = "LavaLauncher -- Version "VERSION"\n\n"
                             "Usage: lavalauncher [options...]\n"
@@ -60,61 +61,6 @@ static const char usage[] = "LavaLauncher -- Version "VERSION"\n\n"
                             "  -S <size>                         All border sizes.\n"
                             "  -S <top> <right> <bottom> <left>  Individual border sizes.\n"
                             "  -v                                Verbose output.\n";
-
-/* No-Op function. */
-static void noop () {}
-
-static void output_handle_scale (void *raw_data, struct wl_output *wl_output,
-		int32_t factor)
-{
-	struct Lava_output *output = (struct Lava_output *)raw_data;
-	output->scale              = factor;
-
-	if (output->data->verbose)
-		fprintf(stderr, "Output update scale: s=%d\n", output->scale);
-
-	configure_surface(output->data, output);
-	render_bar_frame(output->data, output);
-}
-
-static const struct wl_output_listener output_listener = {
-	.scale    = output_handle_scale,
-	.geometry = noop,
-	.mode     = noop,
-	.done     = noop
-};
-
-static void xdg_output_handle_name (void *raw_data,
-		struct zxdg_output_v1 *xdg_output, const char *name)
-{
-	struct Lava_output *output = (struct Lava_output *)raw_data;
-	output->name               = strdup(name);
-
-	if (output->data->verbose)
-		fprintf(stderr, "XDG-Output update name: name=%s\n", name);
-}
-
-static void xdg_output_handle_logical_size (void *raw_data,
-		struct zxdg_output_v1 *xdg_output, int32_t w, int32_t h)
-{
-	struct Lava_output *output = (struct Lava_output *)raw_data;
-	output->w                  = w;
-	output->h                  = h;
-
-	if (output->data->verbose)
-		fprintf(stderr, "XDG-Output update logical size: w=%d h=%d\n",
-				w, h);
-
-	render_bar_frame(output->data, output);
-}
-
-static const struct zxdg_output_v1_listener xdg_output_listener = {
-	.logical_size     = xdg_output_handle_logical_size,
-	.name             = xdg_output_handle_name,
-	.logical_position = noop,
-	.description      = noop,
-	.done             = noop
-};
 
 static void registry_handle_global (void *raw_data, struct wl_registry *registry,
 		uint32_t name, const char *interface, uint32_t version)
@@ -152,65 +98,18 @@ static void registry_handle_global (void *raw_data, struct wl_registry *registry
 	else if (! strcmp(interface, wl_seat_interface.name))
 	{
 		if (! create_seat(data, registry, name, interface, version))
-			return;
+			goto error;
 	}
 	else if (! strcmp(interface, wl_output_interface.name))
 	{
-		if (data->verbose)
-			fputs("Adding output.\n", stderr);
-
-		struct wl_output *wl_output = wl_registry_bind(registry, name,
-				&wl_output_interface, 3);
-		struct Lava_output *output = calloc(1, sizeof(struct Lava_output));
-		if ( output == NULL )
-		{
-			fputs("ERROR: Could not allocate.\n", stderr);
-			data->loop = false;
-			data->ret  = EXIT_FAILURE;
-			return;
-		}
-
-		output->data        = data;
-		output->global_name = name;
-		output->wl_output   = wl_output;
-		output->xdg_output  = zxdg_output_manager_v1_get_xdg_output(
-				data->xdg_output_manager, output->wl_output);
-		output->configured  = false;
-
-		wl_list_insert(&data->outputs, &output->link);
-		wl_output_set_user_data(wl_output, output);
-		wl_output_add_listener(wl_output, &output_listener, output);
-		zxdg_output_v1_add_listener(output->xdg_output,
-				&xdg_output_listener, output);
-
-		/* Roundtrip to allow the output handlers to "catch up", as
-		 * the outputs dimensions as well as name are needed for
-		 * creating the bar.
-		 */
-		if ( wl_display_roundtrip(data->display) == -1 )
-		{
-			fputs("ERROR: Roundtrip failed.\n", stderr);
-			data->loop = false;
-			data->ret  = EXIT_FAILURE;
-			return;
-		}
-
-		assert(output->name);
-		if ( data->layer_shell == NULL )
-			return;
-
-		/* If either the name of output equals only_output or if no
-		 * only_output has been given, create a surface for this new
-		 * output.
-		 */
-		if ( data->only_output == NULL
-				|| ! strcmp(output->name, data->only_output) )
-			if (! create_bar(data, output))
-			{
-				data->loop = false;
-				data->ret  = EXIT_FAILURE;
-			}
+		if (! create_output(data, registry, name, interface, version))
+			goto error;
 	}
+
+	return;
+error:
+	data->loop = false;
+	data->ret  = EXIT_FAILURE;
 }
 
 static void registry_handle_global_remove (void *raw_data,

839f27df38a4df2a0654bf117e3e7a4dd77bc2ff
Leon Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2020-05-08 16:50:03 +0200

    Remove my email from copyright header
    It does not belong there and only encourages people to write me directly
    instead of using the mailinglist.

--- src/cursor.c
+++ src/cursor.c
@@ -1,7 +1,7 @@
 /*
  * LavaLauncher - A simple launcher panel for Wayland
  *
- * Copyright (C) 2020 Leon Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
+ * Copyright (C) 2020 Leon Henrik Plickat
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
